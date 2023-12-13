bc970b68cdffb78b82989ada97c58b77f1bbc606
Uexcel2022 <excellenceudoka@gmail.com>
2023-12-13 15:58:00 +0100

    commit

---
+++ src/main/java/com/uexcel/registerloginapp/RegisterLoginAppApplication.java
@@ -0,0 +1,13 @@
+package com.uexcel.registerloginapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RegisterLoginAppApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RegisterLoginAppApplication.class, args);
+    }
+
+}
---
+++ src/main/java/com/uexcel/registerloginapp/entity/User.java
@@ -0,0 +1,22 @@
+package com.uexcel.registerloginapp.entity;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Table(name="users", uniqueConstraints =
+@UniqueConstraint(name="email_unique",columnNames = {"email"}))
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String FullName;
+    private String email;
+    private String role;
+
+}
